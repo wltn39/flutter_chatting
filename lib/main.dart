de24fbdfb671628f24eb2032daa636cf7cb3b2c6
gino <wltn39@gmail.com>
2023-06-29 07:17:11 +0900

    chatting app first

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/main_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginSignupScreen(),
+    );
+  }
+}
